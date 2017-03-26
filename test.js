194f467308c9e5735450056f92f9b7f50ed25dbf
kNelakonda <kavi.nelakonda@gmail.com>
2017-03-25 19:06:49 -0700

    more changs

--- test.js
+++ test.js
@@ -5,4 +5,61 @@ function getName(){
 	console.log(name);
 	selectedFile = $("#javafile")[0].files[0];
 	console.log(selectedFile);
+	var reader = new FileReader();
+	var text;
+	var variableSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	reader.onload = function(e) {
+		
+		//console.log(reader.result);
+		var textName = reader.result.replace("Jessica Jiang", name);
+		console.log(textName);
+		
+		
+		//next stuff here
+		var textVariables = replaceVariables(textName, variableSet);
+		console.log(textVariables);
+	}
+	reader.readAsText(selectedFile);
+
+}
+
+function replaceVariables(originalText, variableSet) {
+	var index = 0;
+	var newText = originalText;
+	var pieces = originalText.split("  int ");
+	for(var i = 1; i< pieces.length; i++){
+		var varName = pieces[i].substring(0, pieces[i].indexOf(" "));
+		console.log(varName);
+		if(varName.length > 1)
+		{
+			newText = replaceAll(newText, varName, variableSet[index] );
+			console.log(newText);
+			index++;
+			if (index >= variableSet){
+				index=0;
+			}
+		}
+		
+	}
+	return newText;
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+function removeVowels(str){
+	var vowels = ["a", "e", "i", "o", "u"];
+	var newStr = str;
+	for(vowel in vowels){
+		newStr = newStr.replace(vowel, "");
+	}
+	if(newStr.length == 0){
+		return str;
+	}
+	else{
+		return newStr;
+	}
 }
---
+++ test0.java
@@ -0,0 +1,6 @@
+  int asdf = 0;
+  int qwer = 0;
+  int asdf = 0;
+  int jkl = 0;
+  int uio = 0;
+  int jkl = 0;
